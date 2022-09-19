f19689c722ec99c1c915a36881e6ea58061599a1
Redstoneur <alfa.aog36@gmail.com>
2022-09-19 22:07:29 +0200

    Version : v1.0.1

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ import os
 
 import discord as d
 
+print(os.environ['TOKEN'])
+
 
 class MyClient(d.Client):
     async def on_ready(self):
@@ -22,6 +24,26 @@ class MyClient(d.Client):
 intents = d.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
+client = d.Client(intents=intents)
+
+
+@client.event
+async def on_ready(self):
+    print('Logged in as')
+    print(self.user.name)
+    print(self.user.id)
+    print('------')
+
+
+@client.event
+async def on_message(self, message):
+    # we do not want the bot to reply to itself
+    if message.author.id == self.user.id:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello {0.author.mention}'.format(message))
+
+
 # variable système pour le token
 client.run(os.environ['TOKEN'])
