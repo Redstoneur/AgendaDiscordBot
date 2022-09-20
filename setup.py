dfeebda7acd7bdea1845f527ffbca034b5213efa
Redstoneur <alfa.aog36@gmail.com>
2022-09-20 09:23:12 +0200

    Version : v1.0.1

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import os
 import discord as d
 
 token: str = os.environ['TOKEN']
+
 if token is None:
     raise ValueError('Token not set')
 else:
@@ -14,17 +15,17 @@ intents.message_content = True
 client: d.Client = d.Client(intents=intents)
 
 
-# @client.event
-# async def on_ready():
-#     print('Logged in as {0.user}'.format(client))
-#
-#
-# @client.event
-# async def on_message(message: d.Message):
-#     if message.author == client.user:
-#         return
-#     if message.content.startswith('!hello'):
-#         await message.channel.send('Hello!')
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message: d.Message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
 
 
 client.run(token)
